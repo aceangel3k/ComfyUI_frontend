f65d8fe202f0e62fe79ecbfc8952a452dccc3cf9
Austin Mroz <austin@comfy.org>
2025-11-21 11:45:04 -0800

    Allow graph navigation by browser forward/backward

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -57,6 +57,7 @@ import { useNodeOutputStore } from '@/stores/imagePreviewStore'
 import { KeyComboImpl, useKeybindingStore } from '@/stores/keybindingStore'
 import { useModelStore } from '@/stores/modelStore'
 import { SYSTEM_NODE_DEFS, useNodeDefStore } from '@/stores/nodeDefStore'
+import { useSubgraphNavigationStore } from '@/stores/subgraphNavigationStore'
 import { useSubgraphStore } from '@/stores/subgraphStore'
 import { useWidgetStore } from '@/stores/widgetStore'
 import { useWorkspaceStore } from '@/stores/workspaceStore'
@@ -1315,6 +1316,7 @@ export class ComfyApp {
       workflow,
       this.rootGraph.serialize() as unknown as ComfyWorkflowJSON
     )
+    useSubgraphNavigationStore().updateHash()
     requestAnimationFrame(() => {
       this.canvas.setDirty(true, true)
     })
